46b896068bb6fdb01d72cec9f0da557c9528aa75
FRANCISCONUNES09 <francisconunes76558@gmail.com>
2025-09-29 12:10:39 -0300

    Página de produtos

---
+++ src/components/AsideFilters/index.tsx
@@ -0,0 +1,60 @@
+import CustomButton from "../CustomButton"
+import { Slider } from "../ui/slide"
+
+export default function AsideFilters(){
+    const categories = [
+        {id: 1, name: 'Todos'},
+        {id: 2, name: 'Smartphones'},
+        {id: 3, name: 'Laptops'},
+        {id: 4, name: 'Fones'},
+        {id: 5, name: 'Smartwatch'},
+        {id: 6, name: 'Câmeras'},
+        {id: 7, name: 'Games'},
+    ]
+
+
+    return (
+        <aside className="w-80 flex shrink-0">
+            <div className="rounded-lg border border-[#343942] bg-[#181B20]">
+                <div className="p-6">
+                    <h3 className="text-lg font-semibold mb-6">
+                        Filtros
+                    </h3>
+                    <div className="space-y-6">
+                        <div>
+                            <h3 className="font-medium mb-3">Categoria</h3>
+                            <div className="space-y-2">
+                                {categories.map((category) => {
+                                    return (
+                                        <CustomButton
+                                            key={category.id}
+                                            variant={category.id === 1 ? "primary" : "ghost"}
+                                            className="w-full justify-start h-[40px] text-sm pl-6"
+                                            width="min-w-[300px]"
+                                        >
+                                            {category.name}
+                                        </CustomButton>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <h3 className="font-medium mb-3">Faixa de preço</h3>
+                            <div className="spcae-y-4">
+                                <Slider 
+                                    max={20000}
+                                    step={100}
+                                    className="w-full"
+                                />
+                                <div className="flex justify-between text-sm">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    )
+}
--- src/pages/products/index.tsx
+++ src/pages/products/index.tsx
@@ -1,26 +1,51 @@
+import AsideFilters from "@/components/AsideFilters";
 import CustomInput from "@/components/Custominpunt";
 import { PageWrapper } from "@/components/PageWrapper";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { IoSearch } from "react-icons/io5";
 
 export default function Products() {
-    return (
-        <PageWrapper.Root>
-            <PageWrapper.Header />
+  return (
+    <PageWrapper.Root>
+      <PageWrapper.Header />
 
-            <PageWrapper.content
-                title="Todos os produtos"
-                subtitle="8 produtos encontrados"
-            >
-                <div className="mb-8 space-y-4">
-                    <div className="flex flex-col md:flex-row gap-4">
-                        <CustomInput
-                            type="text"
-                            placeholder="Buscar produtos"
-                            icon={<IoSearch />}
-                        />
-                    </div>
-                </div>
-            </PageWrapper.content>
-        </PageWrapper.Root>
-    )
+      <PageWrapper.content
+        title="Todos os produtos"
+        subtitle="8 produtos encontrados"
+      >
+        <div className="mb-8 space-y-4">
+          <div className="flex flex-col md:flex-row gap-4">
+                <CustomInput
+                    type="text"
+                    placeholder="Buscar produtos"
+                    icon={<IoSearch />}
+                />
+
+                <Select>
+                    <SelectTrigger className="w-48">
+                        <SelectValue placeholder="Ordenar por" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="name">Nome A-Z</SelectItem>
+                        <SelectItem value="price-low">Menor preço</SelectItem>
+                        <SelectItem value="price-high">Maior preço</SelectItem>
+                        <SelectItem value="rating">Melhor avaliação</SelectItem>
+                        <SelectItem value="reviews">Mais avaliados</SelectItem>
+                    </SelectContent>
+                </Select>
+          </div>
+        </div>
+
+        <div className="flex gap-8">
+            <AsideFilters />
+        </div>
+      </PageWrapper.content>
+    </PageWrapper.Root>
+  );
 }
